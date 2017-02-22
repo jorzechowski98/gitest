0d0844fdd5342db3daf144f5224ae1636935d973
jorzechowski98 <jorzechowski98@gmail.com>
2017-02-22 14:16:39 +0100

    Update konto01.py

--- python/konto01.py
+++ python/konto01.py
@@ -6,6 +6,6 @@ ile = int(raw_input("Wpłata: "))
 bilans += ile
 print "Stan konta:", bilans
 
-ile = int(raw_input("Wypłata: "))
+ile = int(raw_input("Stojek: "))
 bilans -= ile
 print "Stan konta:", bilans
